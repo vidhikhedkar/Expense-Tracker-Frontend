30ebe3e009ca289ffa2b8a9a2798bee6a5964b17
vidhikhedkar <vidhikhedkar7@gmail.com>
2026-07-10 12:47:05 +0530

    add

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { getProfile } from "../api/api";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  if (!token) return <Navigate to="/login" />;
-  return children;
+  const [loading, setLoading] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await getProfile();
+        setAuthenticated(true);
+      } catch (err) {
+        setAuthenticated(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (loading) return <h2>Loading...</h2>;
+
+  return authenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
